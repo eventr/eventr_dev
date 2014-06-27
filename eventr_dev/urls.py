eba7d4ecf6579a9a394d9c76a280bb5abd055450
mlippie <lippeveld.maxim@gmail.com>
2014-06-27 21:41:22 +0200

    urls aangepast

--- eventr_dev/urls.py
+++ eventr_dev/urls.py
@@ -2,7 +2,5 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 ]
